fe9465c78005bf27e3f5592781c67f68721f7a9d
Robbie <txt3rob@gmail.com>
2015-07-20 14:22:43 +0100

    More updates
    Updates

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 	<title>Magic Mirror</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<meta http-equiv="cache-control" content="max-age=0" />
+		<meta http-equiv="cache-control" content="no-cache" />
+		<meta http-equiv="expires" content="0" />
+		<meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+		<meta http-equiv="pragma" content="no-cache" />
 	<style type="text/css">
 		<?php include('css/main.css') ?>
 	</style>
@@ -8,19 +16,14 @@
 	<script type="text/javascript">
 		var gitHash = '<?php echo trim(`git rev-parse HEAD`) ?>';
 	</script>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<meta http-equiv="cache-control" content="max-age=0" />
-		<meta http-equiv="cache-control" content="no-cache" />
-		<meta http-equiv="expires" content="0" />
-		<meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
-		<meta http-equiv="pragma" content="no-cache" />
-	<meta name="google" value="notranslate" />
+	
+	
 </head>
 <body>
 
 	<div class="top left"><div class="date small dimmed"></div><div class="time"></div><div class="calendar xxsmall"></div></div>
 	<div class="top right"><div class="windsun small dimmed"></div><div class="temp"></div><div class="forecast small dimmed"></div></div>
-	<div class="center-ver center-hor"><!-- <div class="dishwasher light">Vaatwasser is klaar!</div> --></div>
+	<div class="center-ver center-hor"></div>
 	<div class="lower-third center-hor"><div class="compliment light"></div></div>
 	<div class="bottom center-hor"><div class="news medium"></div></div>
 
@@ -33,7 +36,7 @@
 <script src="js/config.js"></script>
 <script src="js/rrule.js"></script>
 <script src="js/main.js?nocache=<?php echo md5(microtime()) ?>"></script>
-<!-- <script src="js/socket.io.min.js"></script> -->
+
 
 </body>
 </html>
