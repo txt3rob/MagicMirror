0b3ccde8817ea8421d5131aae8818d5e4d823390
root <root@LA-PI2-TOPPI.(none)>
2015-07-20 14:20:21 +0000

    Fixed some icon issues

--- index.php
+++ index.php
@@ -3,16 +3,18 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 	<title>Magic Mirror</title>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<meta http-equiv="cache-control" content="max-age=0" />
 		<meta http-equiv="cache-control" content="no-cache" />
 		<meta http-equiv="expires" content="0" />
 		<meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
 		<meta http-equiv="pragma" content="no-cache" />
-	<style type="text/css">
-		<?php include('css/main.css') ?>
-	</style>
-	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/weather-icons/1.3.2/css/weather-icons.css">
+		<script type="text/javascript" src="//code.jquery.com/jquery-latest.min.js"></script>
+		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/weather-icons/1.3.2/css/weather-icons.min.css" />
+		<link rel="stylesheet" type="text/css" href="css/main.css" />
+
+	
 	<script type="text/javascript">
 		var gitHash = '<?php echo trim(`git rev-parse HEAD`) ?>';
 	</script>
@@ -29,13 +31,13 @@
 
 </div>
 
-<script src="js/jquery.js"></script>
-<script src="js/jquery.feedToJSON.js"></script>
-<script src="js/ical_parser.js"></script>
-<script src="js/moment-with-langs.min.js"></script>
-<script src="js/config.js"></script>
-<script src="js/rrule.js"></script>
-<script src="js/main.js?nocache=<?php echo md5(microtime()) ?>"></script>
+
+<script type="text/javascript" src="js/jquery.feedToJSON.js"></script>
+<script type="text/javascript" src="js/ical_parser.js"></script>
+<script type="text/javascript" src="js/moment-with-langs.min.js"></script>
+<script type="text/javascript" src="js/config.js"></script>
+<script type="text/javascript" src="js/rrule.js"></script>
+<script type="text/javascript" src="js/main.js?nocache=<?php echo md5(microtime()) ?>"></script>
 
 
 </body>
