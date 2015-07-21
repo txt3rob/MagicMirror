3a4d1507c0850762f7d495fd7224a79c83975623
Robbie <txt3rob@gmail.com>
2015-07-21 09:31:16 +0000

    fade train times

--- js/trains.js
+++ js/trains.js
@@ -1,3 +1,4 @@
+function trains(){
 $.post( "trains.php", { to: "LIV", from: "WKI" })
   .done(function( data ) {
     obj = JSON.parse(data);
@@ -8,9 +9,21 @@ var i = 0;
 var len = obj.length;
 var text2 = "";
 
+op = 1;
+j = 1;
+highestval = 10;
+highestvalop = highestval+1;
+
+
+
+
 for (; i < len; ) {
-    text2 += "<img src='./images/whitetrain.png' id='train-icon'>&nbsp;" + obj[i][1] + "&nbsp;" + obj[i][3] + "<br>";
+op = op - (j/highestvalop);
+
+    text2 += "<div style='opacity:" + op.toFixed(1) + "'><img src='./images/whitetrain.png' id='train-icon'>&nbsp;" + obj[i][1] + "&nbsp;" + obj[i][3] + "</div>";
     i++;
 }
 document.getElementById("trains").innerHTML = "Trains to " + obj[0][2] + "<Br>" + text2;
 });
+}
+setTimeout(trains(), 3600);
